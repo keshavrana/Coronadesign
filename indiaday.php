d1324fdb1db2885de0b39ae8ef91043198130a2d
keshav <keshavrana396@gmail.com>
2020-04-20 23:55:38 +0530

    second commit

--- indiaday.php
+++ indiaday.php
@@ -54,7 +54,7 @@
 
           
 
-          <li class="nav-logo"><a href="index.html"><img src="images/bg4.png" alt="" class="img-fluid"></a></li>
+          <li class="nav-logo"><a href="index.php"><img src="images/bg4.png" alt="" class="img-fluid"></a></li>
 
           
           <li><a href="indiaday.php">INDIADAYWISE</a></li>
