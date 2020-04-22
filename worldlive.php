e7f33ed9593ddafef1608cb79b3051386b63de88
keshav <keshavrana396@gmail.com>
2020-04-22 11:21:15 +0530

    second

--- worldlive.php
+++ worldlive.php
@@ -29,12 +29,7 @@
 
 
 
-  <!-- =======================================================
-  * Template Name: Knight - v2.0.
-  * Template URL: https://bootstrapmade.com/knight-free-bootstrap-theme/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
+
 </head>
 
 <body onload="fetch()">
@@ -170,10 +165,7 @@ function fetch(){
         &copy; Copyright <strong><span>@2020</span></strong>. All Rights Reserved
       </div>
       <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/knight-free-bootstrap-theme/ -->
+
         Designed by <a href="https://keshavtechnical.netlify.com/">Keshav Rana</a>
       </div>
     </div>
