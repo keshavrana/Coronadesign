0063e245a6c30c042cb79478c032be178d0fb5ab
keshav <keshavrana396@gmail.com>
2020-04-23 19:13:20 +0530

    second

--- index.php
+++ index.php
@@ -412,7 +412,7 @@ while($i < $statescount){
 
           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
             <div class="portfolio-wrap">
-              <img src="images/image4.jpg" class="img-fluid" alt="">
+              <img src="https://www.mygov.in/sites/default/files/master_image/volunteertn.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
               <p>GOVERMENT OF INDIA</p>
                 <div class="portfolio-links">
